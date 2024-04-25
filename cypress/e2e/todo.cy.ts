6cb589614103e727c365c827c586fb38220156b9
rdg11 <rgoodbody11@gmail.com>
2024-04-24 19:15:38 -0500

    Revert "Add cypress tests"
    This reverts commit 4efd9d4a4cdf13c3f83f5b4d6cc5c885df44ec95.

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -25,42 +25,6 @@ describe("todo", () => {
     cy.get('[data-cy="save"]').click();
   });
 
-  it("should check off the todo", () => {
-    cy.visit("http://localhost:4200");
-    cy.get('[data-cy="todo-wrapper"]')
-      .contains("Wash the dishes")
-      .parents('[data-cy="todo-wrapper"]')
-      .find('[data-cy="todo-complete"]')
-      .click();
-    cy.get('[data-cy="apply"]').click();
-  });
-
-  it("should reset the todo", () => {
-    cy.visit("http://localhost:4200");
-    cy.get('[data-cy="todo-wrapper"]')
-      .contains("Wash the dishes")
-      .parents('[data-cy="todo-wrapper"]')
-      .find('[data-cy="todo-reset"]')
-      .click();
-    cy.get('[data-cy="apply"]').click();
-  });
-
-  it("should filter the todos by label", () => {
-    cy.visit("http://localhost:4200");
-    cy.get('[data-cy="filter-type-dropdown"]').click();
-    cy.get('[data-cy="filter-type"]').contains("Label").click();
-    cy.get('[data-cy="filter-input"]').type("Wash").wait(1000);
-    cy.get('[data-cy="todo-wrapper"]').contains("Wash").should("be.visible");
-  });
-
-  it("should filter the todos by category", () => {
-    cy.visit("http://localhost:4200");
-    cy.get('[data-cy="filter-type-dropdown"]').click();
-    cy.get('[data-cy="filter-type"]').contains("Category").click();
-    cy.get('[data-cy="filter-input"]').type("house").wait(1000);
-    cy.get('[data-cy="todo-wrapper"]').contains("Wash").should("be.visible");
-  });
-
   it("should remove the sample todo", () => {
     cy.visit("http://localhost:4200");
     cy.get('[data-cy="todo-wrapper"]')
